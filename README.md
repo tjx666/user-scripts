f35941f3c9c9313624cf5bd74aeb1c1ae3ffca4b
YuTengjing <ytj2713151713@gmail.com>
2024-03-14 18:11:34 +0800

    feat: 新脚本 `重新定义 Boss 直聘`

--- README.md
+++ README.md
@@ -7,8 +7,16 @@
 | 脚本名                              | 从 GIthub 安装           | 从 GreasyFork 安装       |
 | ----------------------------------- | ------------------------ | ------------------------ |
 | [屏蔽 Boss 直聘猎头岗位][bh-github] | [Install][bh-github-raw] | [Install][bh-greasyfork] |
-|                                     |                          |                          |
+| [重新定义 Boss 直聘][rb-github]     | [Install][rb-github-raw] | [Install][rb-greasyfork] |
 
 [bh-github]: https://github.com/tjx666/user-scripts/blob/main/block-hunter.user.js
 [bh-github-raw]: https://raw.githubusercontent.com/tjx666/user-scripts/main/block-hunter.user.js
 [bh-greasyfork]: https://greasyfork.org/zh-CN/scripts/489722-%E5%B1%8F%E8%94%BD-boss-%E7%9B%B4%E8%81%98%E7%8C%8E%E5%A4%B4%E5%B2%97%E4%BD%8D
+[rb-github]: https://github.com/tjx666/user-scripts/blob/main/refined-boss.user.js
+[rb-github-raw]: https://raw.githubusercontent.com/tjx666/user-scripts/main/refined-boss.user.js
+[rb-greasyfork]: https://greasyfork.org/zh-CN/scripts/489722-%E5%B1%8F%E8%94%BD-boss-%E7%9B%B4%E8%81%98%E7%8C%8E%E5%A4%B4%E5%B2%97%E4%BD%8D
+
+## 致谢
+
+- [Aloea](blog.liluhui.cn) 的脚本 [BOSS 直聘信息透出](https://greasyfork.org/zh-CN/scripts/486545-boss%E7%9B%B4%E8%81%98%E4%BF%A1%E6%81%AF%E9%80%8F%E5%87%BA)
+  提供获取岗位信息的思路
---
+++ refined-boss.user.js
@@ -0,0 +1,134 @@
+// ==UserScript==
+// @name         重新定义Boss直聘
+// @namespace    http://tampermonkey.net/
+// @version      0.0.0
+// @description  显示岗位最后修改时间，屏蔽不活跃岗位
+// @author       YuTengjing
+// @supportURL   https://github.com/tjx666/user-scripts/issues
+// @homepage     https://github.com/tjx666/user-scripts
+// @match        https://www.zhipin.com/web/geek/job*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zhipin.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    async function sleep(ms) {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(), ms);
+        });
+    }
+
+    const day = 1000 * 60 * 60 * 24;
+    const week = day * 7;
+    const month = week * 4;
+    function humanizeDuration(ms) {
+        if (ms <= day) {
+            return '一天内';
+        } else if (ms <= day * 3) {
+            return '三天内';
+        } else if (ms <= week) {
+            return '本周内';
+        } else if (ms <= week * 2) {
+            return '两周内';
+        } else if (ms <= month) {
+            return '本月内';
+        } else if (ms <= month * 3) {
+            return '最近三个月';
+        } else {
+            return '超过三个月';
+        }
+    }
+
+    function getDurationColor(ms) {
+        if (ms <= week) return '#fe574a';
+        else if (ms <= month) return '#40b14f';
+        else return '#666';
+    }
+
+    function getJobListUrls() {
+        return window.performance
+            .getEntries()
+            .filter(
+                (item) =>
+                    item.contentType === 'application/json' && item.name.includes('/joblist.json?'),
+            )
+            .map((item) => item.name);
+    }
+
+    /**
+     * 翻页，搜索别的岗位都会请求 jobList
+     */
+    const listenUpdateJobList = (function () {
+        const callbacks = [];
+        let lastUpdateKey = '';
+
+        function checkUpdate() {
+            if (callbacks.length === 0) return;
+
+            const jobListUrls = getJobListUrls();
+            if (jobListUrls.length === 0) return;
+
+            if (jobListUrls.at(-1) !== jobListUrls.at(-2)) {
+                const updateKey = `${jobListUrls.at(-2)}->${jobListUrls.at(-1)}`;
+                if (updateKey !== lastUpdateKey) {
+                    (async function () {
+                        const resp = await fetch(jobListUrls.at(-1));
+                        const jobLists = (await resp.json()).zpData.jobList;
+                        callbacks.forEach((cb) => cb(jobLists));
+                    })();
+                }
+                lastUpdateKey = updateKey;
+            }
+        }
+
+        (async function () {
+            while (true) {
+                checkUpdate();
+                await sleep(100);
+            }
+        })();
+
+        /**
+         * @param {(jobList: any[]) => void} callback
+         */
+        return function (callback) {
+            callbacks.push(callback);
+            checkUpdate();
+        };
+    })();
+
+    async function main() {
+        listenUpdateJobList((jobList) => {
+            const now = Date.now();
+            const jobMap = jobList.reduce((map, cur) => {
+                map.set(cur.encryptJobId, cur);
+                return map;
+            }, new Map());
+            const jobCardLinks = document.querySelectorAll('a.job-card-left');
+            for (const link of jobCardLinks) {
+                const jobId = link.href.match(/job_detail\/(.*?)\.html/)?.[1];
+                const job = jobMap.get(jobId);
+                if (!job) continue;
+
+                const duration = now - job.lastModifyTime;
+                if (duration > month * 3) {
+                    link.parentElement.parentElement.remove();
+                    continue;
+                }
+
+                const jobTitle = link.querySelector('.job-title');
+                const modDateSpan = document.createElement('span');
+                modDateSpan.className = 'mod-date';
+                modDateSpan.innerHTML = '&emsp;' + humanizeDuration(duration);
+                modDateSpan.style = `font-size: 13px; font-weight: normal; color: ${getDurationColor(
+                    duration,
+                )};`;
+                jobTitle.append(modDateSpan);
+            }
+        });
+    }
+
+    main();
+})();
